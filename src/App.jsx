78461c6f56e860a7d63ad9eef2920a8f06ad4890
BijoyDeep11 <bijoydeep007@gmail.com>
2025-12-05 16:24:56 +0530

    set up appwrite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  
+  
+  return (
+    <>
+      <div>BLOG APP</div>
+    </>
+  )
+}
+
+export default App
