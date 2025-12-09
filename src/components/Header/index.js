d67aead2b44cc47e633c034e1c2c63830235ae9e
BijoyDeep11 <bijoydeep007@gmail.com>
2025-12-09 20:11:59 +0530

    components added

--- src/components/Header/index.js
+++
@@ -1,7 +0,0 @@
-import Header from './Header.jsx';
-import Footer from '../Footer/Footer.jsx';
-
-export{
-    Header,
-    Footer
-}
---
+++ src/components/Input.jsx
@@ -0,0 +1,24 @@
+import React, {useId} from 'react'
+
+const Input = React.forwardRef( function Input({
+    label,
+    type = "text",
+    className = "",
+    ...props
+}, ref){
+    const id = useId()
+    return (
+    <div className='w-full'>
+        {label && <label htmlFor={id} className='block mb-2 text-sm font-medium text-gray-900'>{label}</label>}
+        <input
+        type={type}
+        id={id}
+        className={`border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ${className}`}
+        ref={ref}
+        {...props}
+        />
+    </div>
+    )
+})
+
+export default Input
