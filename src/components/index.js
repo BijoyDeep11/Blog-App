fcca71eadc02f4bc02f0f38f3c73e11ebb49bf88
BijoyDeep11 <bijoydeep007@gmail.com>
2025-12-12 20:15:37 +0530

    Real-time editor and postform initialised

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,12 @@ import Footer from './Footer/Footer.jsx';
 import Container from './container/Container.jsx';
 import Logo from './Logo.jsx';
 import LogoutBtn from './Header/LogoutBtn.jsx';
-
+import RTE from "./RTE"
 export{
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn, 
+    RTE
 }
