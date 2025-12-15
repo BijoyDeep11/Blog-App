c1865a4a47e72f8683a1066ee1422cd124b08cdf
BijoyDeep11 <bijoydeep007@gmail.com>
2025-12-15 09:17:07 +0530

    debugging

--- src/components/index.js
+++ src/components/index.js
@@ -1,24 +1,30 @@
-import Header from './Header.jsx';
-import Footer from './Footer/Footer.jsx';
-import Container from './container/Container.jsx';
-import Logo from './Logo.jsx';
-import LogoutBtn from './Header/LogoutBtn.jsx';
-import RTE from "./RTE"
-import Signup from './Signup.jsx';
-import Login from './Login.jsx';
-import PostForm from './post-form/PostForm.jsx';
-import PostCard from './PostCard.jsx'
-import AuthLayout from "./AuthLayout.jsx"
-export{
+import Select from "./Header/Select";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import Button from "./Button";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Input from "./Input";
+export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn, 
+    LogoutBtn,
     RTE,
     Signup,
     Login,
+    Button,
     PostForm,
     PostCard,
-    AuthLayout
+    AuthLayout, 
+    Input,
+    Select,
 }
